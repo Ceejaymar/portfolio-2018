293d7d2f5f5c19354304dbd4faeb58080e7d4466
Carlos Martinez <martinezcj2@gmail.com>
2018-09-04 01:09:53 -0400

    Setup up src folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import Routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Routes />, document.getElementById('root'));
